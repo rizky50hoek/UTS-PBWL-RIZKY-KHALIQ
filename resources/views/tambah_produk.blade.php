3c19caad6c101d1043c142cb0f103c0640c28259
rizky khaliq <rizkykhaliq06@gmail.com>
2025-11-02 11:31:41 +0700

    Tugas 5: Menampilkan dropdown Jenis Produk dari controller menggunakan array dan perulangan

--- resources/views/tambah_produk.blade.php
+++ resources/views/tambah_produk.blade.php
@@ -7,28 +7,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand fw-bold" href="#">UTS Laravel</a>
+    <div class="container mt-4">
+        <div class="d-flex justify-content-between align-items-center mb-3">
+            <h4 class="mb-0">UTS Laravel</h4>
         </div>
-    </nav>
 
-    <div class="container mt-5">
         <h2 class="text-center mb-4">Form Tambah Produk</h2>
 
         <form>
             <div class="row mb-3">
-                <div class="col-md-4">
+                <div class="col">
                     <label for="kode" class="form-label">Kode Produk</label>
-                    <input type="text" class="form-control" id="kode" placeholder="Input Kode Produk">
+                    <input type="text" id="kode" name="kode" class="form-control" placeholder="Input Kode Produk">
                 </div>
-                <div class="col-md-4">
+                <div class="col">
                     <label for="nama" class="form-label">Nama Produk</label>
-                    <input type="text" class="form-control" id="nama" placeholder="Input Nama Produk">
+                    <input type="text" id="nama" name="nama" class="form-control" placeholder="Input Nama Produk">
                 </div>
-                <div class="col-md-4">
+                <div class="col">
                     <label for="jenis" class="form-label">Jenis Produk</label>
-                    <select class="form-select" id="jenis">
+                    <select id="jenis" name="jenis" class="form-select">
                         <option selected>Pilih Produk</option>
                         @for ($i = 0; $i < count($jenis); $i++)
                             <option>{{ $jenis[$i] }}</option>
@@ -38,16 +36,14 @@
             </div>
 
             <div class="row mb-3">
-                <div class="col-md-4">
+                <div class="col">
                     <label for="harga" class="form-label">Harga</label>
-                    <input type="text" class="form-control" id="harga" placeholder="Input Harga">
+                    <input type="number" id="harga" name="harga" class="form-control" placeholder="Input Harga">
                 </div>
             </div>
 
-            <div class="text-center">
-                <button type="submit" class="btn btn-success">Simpan</button>
-                <a href="/produk" class="btn btn-secondary ms-2">Kembali</a>
-            </div>
+            <button type="submit" class="btn btn-success">Simpan</button>
+            <a href="/produk" class="btn btn-secondary">Kembali</a>
         </form>
     </div>
 </body>
