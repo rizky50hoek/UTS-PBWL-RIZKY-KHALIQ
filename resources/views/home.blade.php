0b3696243dfe732ae3bf15317bdb32fc19658daa
rizky khaliq <rizkykhaliq06@gmail.com>
2025-11-02 09:57:40 +0700

    Soal 2: Menambahkan sidebar pada halaman Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,53 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>UTS Laravel</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <style>
+    .container {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 30px;
+    }
+    .content {
+      flex: 1;
+      text-align: left;
+      padding-left: 20px;
+    }
+    .sidebar {
+      width: 200px;
+      border-left: 1px solid #ccc;
+      padding-left: 20px;
+    }
+    .sidebar a {
+      display: block;
+      text-decoration: none;
+      color: black;
+      margin-bottom: 8px;
+    }
+    .sidebar input {
+      width: 100%;
+      margin-top: 5px;
+      padding: 5px;
+    }
+  </style>
 </head>
 <body>
+
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">UTS Laravel</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
   </div>
 </nav>
 
-<div class="container text-center mt-5">
-  <h1>Halaman Home</h1>
+<div class="container">
+  <div class="content">
+    <h1>Halaman Home</h1>
+  </div>
+
+  <div class="sidebar">
+    <a href="#">Home</a>
+    <a href="#">Produk</a>
+    <input type="text" placeholder="Search">
+  </div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
